6fe697e7886e5c624399b61c7db9d75c58706963
hanhaihu <739065949@qq.com>
2017-05-17 18:04:11 +0800

    完成保障部分页面

---
+++ src/js/common.js
@@ -0,0 +1,31 @@
+﻿$(function(){
+	$(window).resize(function() {
+		moveRe();
+	});
+	
+	function moveRe(){
+		var winH = $(window).height()
+		var mainH = $("body").height();
+		if(mainH < winH){
+			$(".footer").css({
+				position:"fixed",
+				_position:"absolute",
+				bottom:"0",
+				left:"0",
+				maxHeight:"1200px",
+				width:"100%"
+			})
+		}else{
+			$(".footer").css({
+				position:"static",
+				_position:"static",
+				maxHeight:"1200px",
+				width:"100%"
+			})
+		}
+	}
+	
+	
+	moveRe();
+})
+
